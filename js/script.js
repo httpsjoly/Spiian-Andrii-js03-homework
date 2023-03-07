3e940f24e743fe3c1db1666500d8cad3b3d0bdda
Andrii Spiian <andrey.spiyan@icloud.com>
2023-03-07 12:18:02 +0300

    create lets

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let userName = prompt('What is your name?', '').trim();
+userName = userName[0].toUpperCase() + userName.slice(1).toLowerCase();
+// console.log(userName[0].toUpperCase() + userName.slice(1).toLowerCase());
+
+let lastName = prompt('What is your last name?', '').trim();
+lastName = lastName[0].toUpperCase() + lastName.slice(1).toLowerCase();
