b90570d5dfc52c0485bcd27164f91ddb9c022f01
Andrii Spiian <andrey.spiyan@icloud.com>
2023-03-07 13:25:15 +0300

    settings userEmail

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
 let userName = prompt('What is your name?', '').trim();
 userName = userName[0].toUpperCase() + userName.slice(1).toLowerCase();
-// console.log(userName[0].toUpperCase() + userName.slice(1).toLowerCase());
 
 let lastName = prompt('What is your last name?', '').trim();
 lastName = lastName[0].toUpperCase() + lastName.slice(1).toLowerCase();
+
+console.log(userName, lastName);
+let userEmail = prompt('Enter your Email', "").replaceAll().toLowerCase();
+userEmail = (!userEmail.includes('@')) ? `not valid email <b>${userEmail}</b> (symbol @ not exist)`:
+            (userEmail.startsWith('@')) ? `not valid email <b>${userEmail}</b> (symbol @ find in first place)`:
+            (userEmail.endsWith('@')) ? `not valid email <b>${userEmail}</b> (symbol @ find in last place)`:
+            `</b>${userEmail}</b>`;
+
+
